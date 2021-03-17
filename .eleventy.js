47d257e25b01acf58de4161a1d15a4ef3739dbd4
pgm-timdesae <timdesae@student.ahs.be>
2021-03-17 23:19:00 +0100

    update

--- .eleventy.js
+++ .eleventy.js
@@ -6,7 +6,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
   eleventyConfig.addPassthroughCopy({
-    'src/main.css': 'main.css',
+    'src/style/main.css': 'main.css',
     'src/images' :  '/images'
   });
   return {
